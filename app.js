c76712ce08bc165c99bb7d24b223d12a4bad3bf0
angelajane <generangelajane7@gmail.com>
2023-02-11 12:31:56 +0800

    completed

--- app.js
+++ app.js
@@ -2,32 +2,80 @@ const express = require("express")
 const app = express()
 const path = require('path')
 const cors = require("cors")
+const { default: axios } = require("axios")
+
 
 const PORT = 4000
 let SOIL = 0;
-
+let FILL_TANK = false
+let TANK_LEVEL = 100;
+let FORCE_WATERING = false;
+let CHANCE_RAIN = 0;
+let PRECIPITATION = 0;
 app.use(cors({"origin":"*"}))
 app.use(express.static('public'))
 
 app.listen(PORT, ()=>{
     console.log("App is running on port:" + PORT)
 })
-app.get("/",(req, res)=>{
-    res.send("ok")
+app.get("/ff",(req, res)=>{
+
+    res.send("chance of rain"+CHANCE_RAIN)
 })
 
 app.get("/send-soil/:level",(req, res)=>{
     SOIL = req.params.level
     res.send("ok")
-    console.log("moisture:",SOIL)
 })
-app.get("/get-soil",(req, res)=>{
+app.get("/send-water/:level",(req, res)=>{
+    TANK_LEVEL = req.params.level
+    res.send("ok")
+})
+app.get("/get-soil/:precipt",(req, res)=>{
+    PRECIPITATION = req.params.precipt
     res.send({
         soil:SOIL
     })
-
-
 })
+app.get("/should-water",async (req, res)=>{
+    //var d = new Date("2011-04-20T09:30:51.01");
+    var d = new Date();
+    time = d.getHours()
+
+    let water = false
+
+    if(d===5 || d === 18 && (PRECIPITATION < 0.1 && chance_rain < 90))water = true
+
+    if(FORCE_WATERING || water){
+        if(FORCE_WATERING)console.log("Forcing Water")
+
+        FORCE_WATERING = false
+        res.send("go")
+    }else res.send("no")
+})
+app.get("/should-fill-tank",(req, res)=>{
+    if(FILL_TANK)res.send("go")
+    else res.send("no")
+    FILL_TANK = false
+})
+app.get("/force-water",(req, res)=>{
+    FORCE_WATERING = true
+    console.log("FORCE WATERING:"+FORCE_WATERING)
+    res.send("FORCE WATERING:"+FORCE_WATERING)
+})
+
+app.get("/fill-tank",(req, res)=>{
+    FILL_TANK = true
+    console.log("fill tank")
+    res.send(true)
+})
+app.get("/get-water/:chance_rain",(req, res)=>{
+    CHANCE_RAIN = req.params.chance_rain
+    res.send({
+        level:TANK_LEVEL
+    })
+})
+
 
 
 module.exports = app
--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,17 @@
+const SERVER_URL = "http://192.168.254.104:4000"
+
+
 document.body.onload = async function(){
     const futureForcast = "https://api.weatherapi.com/v1/forecast.json?key=25e554c0b773482cb6b173052220306&q=calinog,iloilo&aqi=no&fbclid=IwAR0Gbr-wBhoEhgjmWDpjHpJcZnWaMAXq3ZdUE9iVwUxz8jywomRo_xucH60&days=1"
-    const soilForcast = "http://192.168.254.146:4000/get-soil"
 
+    
+
+    
     const futureRequest = await fetch(futureForcast)
-    const futureData = await futureRequest.json()
-    console.log(futureData)
+   const futureData = await futureRequest.json()
+   console.log(futureData)
 
-    const soilRequest = await fetch(soilForcast)
-    const soilData = await soilRequest.json()
-    console.log(soilData)
+
 
     const temp = futureData.current.temp_c
     const sky = futureData.current.condition.text
@@ -18,7 +21,33 @@ document.body.onload = async function(){
     document.getElementById("temp").innerText = temp + " ℃"
     document.getElementById("sky").innerText = sky
     document.getElementById("sky_icon").src = icon
-    document.getElementById("moist").innerText = soilData.soil + "%"
+
+
+    const updateValues = async()=>{
+        const soilForcast = SERVER_URL+"/get-soil/"+futureData.current.precip_mm
+        const tankLevel = SERVER_URL+"/get-water/"+futureData.forecast.forecastday[0].hour[0].chance_of_rain
+            const soilRequest = await fetch(soilForcast)
+        const tankRequest = await fetch(tankLevel)
+    
+        const soilData = await soilRequest.json()
+        const tankData = await tankRequest.json()
+        
+        document.getElementById("moist").innerText = soilData.soil + "%"
+        document.getElementById("tank-level").innerText = tankData.level + "%"
+    }
+    setInterval(updateValues,500)
+
+
 
     
+
+    
+}
+async function forceWater(){
+    await fetch(SERVER_URL+"/force-water")
+        alert("watering")
+}
+async function fillTank(){
+    await fetch(SERVER_URL+"/fill-tank")
+        alert("filling tank")
 }
